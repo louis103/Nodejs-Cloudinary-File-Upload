1b14873025dc7b73b3ab0af6090cd1efd6676d97
louis@programmer <62738677+louis103@users.noreply.github.com>
2024-02-16 16:16:38 +0300

    Added single image upload endpoint.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
+const streamifier = require('streamifier');
 
 // cloudinary config
 cloudinary.config({
@@ -20,8 +21,33 @@ app.get("/", (req, res) => {
     res.json({"message": "you have reached the home route!!!"});
 });
 
-// api route for uploading the images
-app.post('/upload', upload.array('images', 3), async (req, res) => {
+// single image upload endpoint
+app.post('/upload-single', upload.single('image'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({error: 'No image provided'});
+        }
+
+        // Create a readable stream from the buffer
+        const stream = cloudinary.uploader.upload_stream({folder: "powerline_incident_images"}, (error, result) => {
+            if (error) {
+                console.error('Error uploading image:', error);
+                res.status(500).json({error: 'Internal Server Error'});
+            } else {
+                const imageUrl = result.secure_url;
+                res.json({url: imageUrl});
+            }
+        });
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+    } catch (error) {
+        console.error('Error uploading image:', error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+// multiple image upload endpoint
+app.post('/upload-multiple', upload.array('images', 3), async (req, res) => {
     try {
         const uploadedPromises = req.files.map(async (file) => {
             return new Promise((resolve, reject) => {
